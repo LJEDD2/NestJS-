553053e86a0f4d2591b13148873e33d4ee1783b4
이정은 <78216102+LJEDD2@users.noreply.github.com>
2024-03-06 15:39:13 +0900

    연습 파일 "실행시켜줘"

---
+++ NestJS연습/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { RankModule } from './rank/rank.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
+
+// 싱글톤 패턴으로 인스턴스 생성 
+//  루트 모듈 
+
+@Module({
+  imports: [RankModule, LeaderboardModule],
+  controllers: [], // [AppController],
+  providers: [], //[AppService],
+})
+
+export class AppModule {}
